4bd959fbe27a75202a08864c85aa4096845c93f7
qeqoos <foxthesaw@gmail.com>
2019-11-02 22:50:44 +0200

    init

---
+++ asdafasf/Assets/Scripts/Accelerometre.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometre : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float movementSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement = new Vector2(Input.acceleration.x, Input.acceleration.y) * movementSpeed;
+        rb.AddForce(movement);
+    }
+    
+}
